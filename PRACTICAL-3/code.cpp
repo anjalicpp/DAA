836eccceee7c11fa85caafda645949bf553b5ef2
anjalicpp <91792558+anjalicpp@users.noreply.github.com>
2022-05-24 21:02:02 +0530

    Create code.cpp

---
+++ PRACTICAL-3/code.cpp
@@ -0,0 +1,4 @@
+Program-3
+Program to find whether a given key element is present in the sorted array or not .
+For an array arr[n],search at the indexes arr[0],arr[2],arr[4],....arr[2^]<key<[2^k+1]
+is found,perform a linear search operation from the index 2^k to find element in the key .
