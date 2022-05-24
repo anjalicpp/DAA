3d1f3d9a6c850ae026ccad74c373b6c3133b146e
anjalicpp <91792558+anjalicpp@users.noreply.github.com>
2022-05-24 21:09:46 +0530

    Create code.cpp

---
+++ PRACTICAL 11/code.cpp
@@ -0,0 +1,5 @@
+Program-11
+Program to sort an array of element by partitioning the arrayinto two sub arrays based on pivot element
+such that one of the subarray holds values smaller than the pivot element .
+Pivot element should be selected randomly from the array .Also find number of comparisons and swaps required 
+for sorting the array.
