21a06bd48f38eb2cf6ce75d4a7f689e0915bf2b2
anjalicpp <91792558+anjalicpp@users.noreply.github.com>
2022-05-24 21:08:07 +0530

    Create code.cpp

---
+++ PRACTICAL-10/code.cpp
@@ -0,0 +1,4 @@
+Program-10
+Program to sort an array of elements by dividing the array into two subarrays
+and combining these subarrays after sorting each one of them.Also find number of comparisons and 
+inversions during sorting the array.
