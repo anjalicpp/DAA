0b286e01f1df95a3c332cb123692f06f5fe65e40
anjalicpp <91792558+anjalicpp@users.noreply.github.com>
2022-05-24 21:02:51 +0530

    Create code.cpp

---
+++ PRACTICAL 4/code.cpp
@@ -0,0 +1,3 @@
+Program-4
+Program to find whether the given key element is present in the array or not .If present
+in the array or not.If present ,then also find the number of copies of given key.
