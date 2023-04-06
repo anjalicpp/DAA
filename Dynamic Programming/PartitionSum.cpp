1ee235c739987708c5d4e36fe5aa8a0793ed88da
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-04-06 20:48:29 +0530

    Update PartitionSum.cpp

--- Dynamic Programming/PartitionSum.cpp
+++ Dynamic Programming/PartitionSum.cpp
@@ -1,1 +1,90 @@
 
+Partition Equal Subset Sum
+MediumAccuracy
+  
+
+Given an array arr[] of size N, check if it can be partitioned into two parts such that the sum of elements in both parts is the same.
+
+Example 1:
+
+Input: N = 4
+arr = {1, 5, 11, 5}
+Output: YES
+Explanation: 
+The two parts are {1, 5, 5} and {11}.
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int subset(int arr[],int n,int sum)
+    {
+        int t[n+1][sum+1];
+        for(int i=0;i<n+1;i++)
+        {
+           for(int j=0;j<sum+1;j++)
+           {
+               if(i==0)
+               t[i][j]=false;
+               if(j==0)
+               t[i][j]=true;
+           }
+        }
+        
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<sum+1;j++)
+            {
+                if(arr[i-1]<=j)
+                {
+                    t[i][j]=t[i-1][j-arr[i-1]]||t[i-1][j];
+                }
+                else t[i][j]=t[i-1][j];
+            }
+        }
+        return t[n][sum];
+        
+    }
+    int equalPartition(int N, int arr[])
+    {
+        int sum=0;
+        for(int i=0;i<N;i++)
+        {
+            sum+=arr[i];
+        }
+        if(sum%2!=0)
+        {
+            return 0;
+        }
+        else 
+        return subset(arr,N,sum/2);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int arr[N];
+        for(int i = 0;i < N;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+        if(ob.equalPartition(N, arr))
+            cout<<"YES\n";
+        else
+            cout<<"NO\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
