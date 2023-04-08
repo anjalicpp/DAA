e57ea354b6d2f59923be6c83ac2dfc301f95ec19
Anjali <91792558+anjalicpp@users.noreply.github.com>
2023-04-08 23:56:18 +0530

    Update Count_ofthe_subset.cpp

--- Dynamic Programming/Count_ofthe_subset.cpp
+++ Dynamic Programming/Count_ofthe_subset.cpp
@@ -1,1 +1,85 @@
+Perfect Sum Problem
+Medium
 
+Given an array arr[] of non-negative integers and an integer sum, the task is to count all subsets of the given array with a sum equal to a given sum.
+
+Note: Answer can be very large, so, output answer modulo 109+7
+
+Example 1:
+
+Input: N = 6, arr[] = {2, 3, 5, 6, 8, 10}
+       sum = 10
+Output: 3
+Explanation: {2, 3, 5}, {2, 8}, {10}
+
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+
+	public:
+	int perfectSum(int arr[], int n, int sum)
+	{
+	    int t[n+1][sum+1];
+	    int m=1000000007;
+	    // Your code goes here
+        for(int i=0;i<n+1;i++)
+        {
+            for(int j=0;j<sum+1;j++)
+            {
+                if(i==0)
+                t[i][j]=0;
+                if(j==0)
+                t[i][j]=1;
+            }
+        }
+        
+       
+	    
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=0;j<sum+1;j++)
+            {
+                if(arr[i-1]<=j)
+                {
+                    t[i][j]=t[i-1][j-arr[i-1]]%m+t[i-1][j]%m;
+                }
+                else t[i][j]=t[i-1][j]%m;
+            }
+        }
+        return t[n][sum]%m;
+	}
+	  
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, sum;
+
+        cin >> n >> sum;
+
+        int a[n];
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+       
+
+	    Solution ob;
+	    cout << ob.perfectSum(a, n, sum) << "\n";
+	     
+    }
+    return 0;
+}
+
+// } Driver Code Ends
